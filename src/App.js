06a8b7d0dddb227fecc3529540d4d32bbc6a5d87
Zhongwei Li <li@Zhongweis-Air.lan>
2022-04-08 20:01:43 -0400

    add routes that directs to debit and credit page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
+import Debits from './components/Debits';
+import Credits from './components/Debits';
 
 class App extends Component {
   constructor() {  // Create and initialize state
@@ -39,6 +41,8 @@ class App extends Component {
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/login" render={LogInComponent}/>
+          <Route exact path="/debits" render={Debits}/>
+          <Route exact path="/credits" render={Credits}/>
         </div>
       </Router>
     );
